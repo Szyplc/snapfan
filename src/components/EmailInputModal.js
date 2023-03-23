62aec5ae911381bc54c572faca60f39ff74ae2c5
Kzyzulek <kzyzulek@gmail.com>
2023-03-23 20:33:27 +0100

    Dzialajaca wersja z otrzymywaniem linku

--- src/components/EmailInputModal.js
+++ src/components/EmailInputModal.js
@@ -1,14 +1,45 @@
 import React, { useState } from "react";
-import { Box, Button, Form, FormField, Heading, Layer, TextInput } from "grommet";
-import { FormClose } from "grommet-icons";
+import { Box, Button, Form, FormField, Heading, Layer, TextInput, Text } from "grommet";
+import { FormClose, Clipboard } from "grommet-icons";
 
 const EmailInputModal = ({ onClose, onSubmit }) => {
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
+  const [link, setLink] = useState("");
+  const [showLink, setShowLink] = useState(false);
 
-  const handleSubmit = () => {
-    onSubmit({ email, name });
-    onClose();
+  const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+
+  const handleSubmit = async () => {
+    if (!emailRegex.test(email)) {
+      alert("Podaj poprawny adres email. Nie będziemy mogli przesłać Ci wyników ankiety");
+      return;
+    }
+    
+    if (name.length < 3) {
+      alert("Imię musi zawierać co najmniej 3 litery");
+      return;
+    }
+    
+    const response = await fetch("http://localhost:3001/generate-link", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ email, name })
+    });
+  
+    const data = await response.json();
+  
+    setLink(data.link);
+    setShowLink(true);
+    console.log(data.link)
+  };
+  
+
+  const handleCopyLink = async () => {
+    await navigator.clipboard.writeText(link);
+    alert("Link został skopiowany do schowka!");
   };
 
   return (
@@ -19,16 +50,26 @@ const EmailInputModal = ({ onClose, onSubmit }) => {
           <Button icon={<FormClose />} onClick={onClose} />
         </Box>
         <Form onSubmit={handleSubmit}>
-          <FormField name="email" label="Email" required>
+          <FormField name="email" label="Email" >
             <TextInput type="email" value={email} onChange={(event) => setEmail(event.target.value)} />
           </FormField>
-          <FormField name="name" label="Imię" required>
+          <FormField name="name" label="Imię" >
             <TextInput value={name} onChange={(event) => setName(event.target.value)} />
           </FormField>
           <Box direction="row" justify="end">
             <Button type="submit" label="Wygeneruj Link" primary />
           </Box>
         </Form>
+        {showLink && (
+            <Box direction="column" align="center" gap="small">
+                <Text>Poniżej znajdziesz link do ankiety:</Text>
+                <Box direction="row" align="center" gap="small">
+                    <TextInput value={link} disabled />
+                    <Button icon={<Clipboard />} onClick={handleCopyLink} />
+                </Box>
+            </Box>
+
+        )}
       </Box>
     </Layer>
   );
--- src/components/QuestionsPage.js
+++ src/components/QuestionsPage.js
@@ -1,6 +1,6 @@
+import './QuestionsPage.css'; // importowanie pliku CSS
 import NavigationBar2 from "./NavigationBar2";
 import Footer2 from "./Footer2";
-
 import React, { useEffect, useState } from "react";
 import { Grommet, Heading, Box, Text, CheckBox, Card } from "grommet";
 import axios from "axios";
@@ -14,105 +14,49 @@ const QuestionsPage = () => {
     });
   }, []);
 
-  const handleCheckBoxChange = (surveyIndex, questionIndex, checked) => {
+  const handleCheckBoxChange = (survey, questionIndex, checked) => {
     const updatedSurveys = [...surveys];
-    updatedSurveys[surveyIndex].questions[questionIndex].answer = checked;
+    updatedSurveys[surveys.indexOf(survey)].questions[questionIndex].answer = checked;
     setSurveys(updatedSurveys);
   };
 
   return (
+    
     <Grommet>
-      <NavigationBar2></NavigationBar2>
+      <NavigationBar2 />
       <Box pad="medium">
-        {surveys.map((survey, surveyIndex) => (
-          <Box key={survey._id} margin={{ vertical: "medium" }}>
+        {surveys.map((survey) => (
+          <Box key={survey.id} margin={{ vertical: "medium" }}>
             <Heading level={3}>{survey.title}</Heading>
-            {survey.questions.map((question, questionIndex) => (
-              <Box key={questionIndex} margin={{ vertical: "small" }}>
-                <Card background="light-1" pad="medium" margin={{bottom: "small"}}>
-                  <Text>{question}</Text>
-                  <Box direction="row" justify="center">
-                    <CheckBox
-                      label="Tak"
-                      checked={question.answer === true}
-                      onChange={(event) =>
-                        handleCheckBoxChange(surveyIndex, questionIndex, event.target.checked)
-                      }
-                    />
-                    <CheckBox
-                      label="Nie"
-                      checked={question.answer === false}
-                      onChange={(event) =>
-                        handleCheckBoxChange(surveyIndex, questionIndex, !event.target.checked)
-                      }
-                    />
-                  </Box>
-                </Card>
-              </Box>
-            ))}
+            {survey.questions.map((question, questionIndex) => {
+              const cardClass = question.answer === true ? "answered-yes" : (question.answer === false ? "answered-no" : "");
+
+              return (
+                <Box key={questionIndex} margin={{ vertical: "small" }}>
+                  <Card className={cardClass} background="light-1" pad="medium" margin={{ bottom: "small" }}>
+                    <Text style={{ textAlign: "center" }}>{question.question}</Text>
+                    <Box direction="row" justify="center" margin={{ right: "20px", left: "20px" }}>
+                      <CheckBox
+                        label="Tak"
+                        checked={question.answer === true}
+                        onChange={(event) => handleCheckBoxChange(survey, questionIndex, event.target.checked)}
+                      />
+                      <CheckBox
+                        label="Nie"
+                        checked={question.answer === false}
+                        onChange={(event) => handleCheckBoxChange(survey, questionIndex, !event.target.checked)}
+                      />
+                    </Box>
+                  </Card>
+                </Box>
+              );
+            })}
           </Box>
         ))}
       </Box>
-      <Footer2></Footer2>
+      <Footer2 />
     </Grommet>
   );
 };
 
 export default QuestionsPage;
-
-
-// import React, { useEffect, useState } from "react";
-// import { Grommet, Heading, Box, Text, CheckBox } from "grommet";
-// import axios from "axios";
-
-// const QuestionsPage = () => {
-//   const [surveys, setSurveys] = useState([]);
-
-//   useEffect(() => {
-//     axios.get("http://localhost:3001/surveys").then((response) => {
-//       setSurveys(response.data);
-//     });
-//   }, []);
-
-//   const handleCheckBoxChange = (surveyIndex, questionIndex, isChecked) => {
-//     setSurveys((prevSurveys) => {
-//       const updatedSurveys = [...prevSurveys];
-//       updatedSurveys[surveyIndex].questions[questionIndex].answer = isChecked;
-//       return updatedSurveys;
-//     });
-//   };
-
-//   return (
-//     <Grommet>
-//       <Box pad="medium">
-//         <Heading level={2}>Ankiety</Heading>
-//         {surveys.map((survey, surveyIndex) => (
-//           <Box key={survey._id} margin={{ vertical: "medium" }}>
-//             <Heading level={3}>{survey.title}</Heading>
-//             {survey.questions.map((question, questionIndex) => (
-//               <Box key={questionIndex} margin={{ vertical: "small" }}>
-//                 <Text>{question}</Text>
-//                 <CheckBox
-//                   label="Tak"
-//                   checked={question.answer === true}
-//                   onChange={(event) =>
-//                     handleCheckBoxChange(surveyIndex, questionIndex, event.target.checked)
-//                   }
-//                 />
-//                 <CheckBox
-//                   label="Nie"
-//                   checked={question.answer === false}
-//                   onChange={(event) =>
-//                     handleCheckBoxChange(surveyIndex, questionIndex, !event.target.checked)
-//                   }
-//                 />
-//               </Box>
-//             ))}
-//           </Box>
-//         ))}
-//       </Box>
-//     </Grommet>
-//   );
-// };
-
-// export default QuestionsPage;
